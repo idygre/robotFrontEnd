a822c270211bdb0dcd6695ecc15c925d05c8ebd2
Your NameGreydi <greydi.lora@yahoo.com>
2022-06-25 14:57:21 -0400

    Finished the Training Results Page

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
 const Results = () => {
-  return <div>now the laptop</div>;
+  return (
+    <div className="trainPage">
+      <div className="trainContainer">
+        <div className="trainTitle">
+          <h1>Training Results</h1>
+        </div>
+        <div className="trainBox">
+          <h1 className="detectedTitle">Predominant colors detected</h1>
+
+          <div className="detected1">
+            <h1 className="colorTitle">Detection #1</h1>
+            <h1 className="colorName">Color</h1>
+            <div className="colorCircle"></div>
+            <h1 className="colorCode">#00000</h1>
+          </div>
+
+          <div className="detected2">
+            <h1 className="colorTitle">Detection #2</h1>
+            <h1 className="colorName">Color</h1>
+            <div className="colorCircle"></div>
+            <h1 className="colorCode">#00000</h1>
+          </div>
+        </div>
+        <div className="trainButtonDiv">
+          <button
+            type="button"
+            name="send-results"
+            id="train"
+            className="trainButton"
+          ></button>
+
+          <div className="label">
+            <label htmlFor="process" className="send-results">
+              Send Results
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Results;
